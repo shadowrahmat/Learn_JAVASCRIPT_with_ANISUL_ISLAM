c7edcf9840809389627ab7a4121f0dcfa6d22279
Rahmat Ullah <sadmanhafizrahmat@gmail.com>
2025-12-24 20:41:36 +0600

    javascript simple code

---
+++ index.js
@@ -0,0 +1,18 @@
+document.write("I love bangladesh");
+console.log("Hello World");
+
+// var fullName = "apple";
+// var fullName = "jackfruits";
+// var fullName = "Mango";
+// console.log(fullName);
+
+let fullName = "apple";
+let fullName2 = "jackfruits";
+let fullName3 = "Mango";
+console.log(fullName);
+
+// personal budget tracker app
+console.log("personal budget tracker app");
+console.log("Rahmat Ullah");
+console.log(120000);
+console.log(5000);
