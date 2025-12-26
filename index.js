d599d4910e450a3d69fad38c433655fe819c367c
Rahmat Ullah <sadmanhafizrahmat@gmail.com>
2025-12-26 18:46:28 +0600

    javascript programming language understand

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-document.write("I love bangladesh");
-console.log("Hello World");
+// document.write("I love bangladesh");
+// console.log("Hello World");
 
 // var fullName = "apple";
 // var fullName = "jackfruits";
 // var fullName = "Mango";
 // console.log(fullName);
 
-let fullName = "apple";
-let fullName2 = "jackfruits";
-let fullName3 = "Mango";
-console.log(fullName);
+// let fullName = "apple";
+// let fullName2 = "jackfruits";
+// let fullName3 = "Mango";
+// console.log(fullName);
 
 // Phase 1
 // personal budget tracker app
 console.log("personal budget tracker app");
-console.log("Rahmat Ullah");
-console.log(120000);
-console.log(5000);
+// console.log("Rahmat Ullah");
+// console.log(120000);
+// console.log(5000);
 
 // phase 2
 // declare variable
@@ -44,7 +44,31 @@ console.log(5000);
 // console.log(typeof(String(134)));
 // console.log(Number(true));
 // console.log(Number(false));
-let Number = 2.123456789;
-console.log(Number.toPrecision(1));
+// let Number = 2.123456789;
+// console.log(Number.toPrecision(1));
 
-// 
+// // concatenate strings
+
+// // document.write("Rahmat" + "Ullah"+ " SUMON");
+
+// let firstName = "Rahmat";
+// let lastName = " ullah";
+// let fullName = firstName + lastName;
+// document.write("MY NAME IS "+fullName + "</br>");
+
+// let  my_Name = "RAHMAT ULLAH ";
+// document.write(my_Name + "is my name");
+
+// let a = 20;
+// let b = 30;
+
+// console.log("a value is = " + a + " and b value is = " + b);
+
+// // let country = "bangladesh";
+// let country = prompt("Just country name : ")
+// let len = country.length;
+// console.log("Country total character = " + len);
+
+let text = "bangladesh";
+let total = text.charAt(2);
+document.write(total);
