c2c72b44ce815638c2e15366f34b2faa6f49b28f
Rahmat Ullah <sadmanhafizrahmat@gmail.com>
2025-12-26 11:55:29 +0600

    string code is here

--- index.js
+++ index.js
@@ -11,8 +11,40 @@ let fullName2 = "jackfruits";
 let fullName3 = "Mango";
 console.log(fullName);
 
+// Phase 1
 // personal budget tracker app
 console.log("personal budget tracker app");
 console.log("Rahmat Ullah");
 console.log(120000);
 console.log(5000);
+
+// phase 2
+// declare variable
+
+// const userName = "Rahmat Ullah";
+// const income = 120000;
+// const expense = 5000;
+
+// console.log(userName); // variable use
+// console.log(income);
+// console.log(expense);
+
+// let num = 41.56789;
+// let num1 = toString(num);
+// let num1 = parseInt(num);
+// let num1 = parseFloat(num);
+// console.log(typeof num1);
+// console.log(num);
+
+// console.log(num.toFixed(4));
+
+// console.log(Number("1234"));
+// console.log(typeof(Number("1234")));
+// console.log(String(134));
+// console.log(typeof(String(134)));
+// console.log(Number(true));
+// console.log(Number(false));
+let Number = 2.123456789;
+console.log(Number.toPrecision(1));
+
+// 
